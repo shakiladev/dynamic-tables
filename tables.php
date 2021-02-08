6da496899a235eb058a4c7d8bafec8e151266dc7
shakiladev <aafonso@ngolait.ao>
2021-02-07 21:31:44 -0300

    Inserting a Css Style and adding a TH html Tag

--- tables.php
+++ tables.php
@@ -3,14 +3,24 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/style.css">
     <title>Dynamic Tables</title>
 </head>
 <body>
+    <h2>Dynamics Tables</h2><br>
     <?php
-        //Generate an Html table
-        // 3 rows, 4 columns
+        //Generate a Company Table
 
-        echo "<table id=\"first\">";
+        echo "<table id=\"company\">";
+
+        echo "<tr>";
+        
+        for ($titles=1; $titles<5; $titles++){
+
+            echo "<th>Title $titles </th>";
+        }
+
+        echo "</tr>";
 
         for ($row=1; $row<4; $row++){
 
@@ -22,6 +32,7 @@
             }
             echo "</tr>";
         }
+        
         echo "</table>";
     ?>
 </body>
