5fba0dcbe5c0ee8563abfbf2d00ec51133747ba3
MartinsSilva <gouveiamartins6@gmail.com>
2021-02-19 09:16:25 +0100

    feature: removi o css padrão, adicionei a cdn do bootstrap para estilos da tabela, e fiz uma refatoração dos loops e apresentação do php, separando por short-tags php

--- tables.php
+++ tables.php
@@ -1,41 +1,47 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt">
 <head>
+    <title>Dynamic Tables</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./css/style.css">
-    <title>Dynamic Tables</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 </head>
 <body>
-    <h2>Dynamics Tables</h2><br>
+    <h2 class="text-center h4 mt-2 mb-5">Dynamics Tables</h2>
+
+    <?php $companyNames = array("Id", "Company", "Address", "Email", "Number"); ?>
+
     <?php
-        //Generate a Company Table
-
-        $companyNames = ["Company", "Adress", "Email", "Number"];
-
-        echo "<table id=\"company\">";
-
-        echo "<tr>";
-        
-        for ($titles=0; $titles<4; $titles++){
-
-            echo "<th> $companyNames[$titles] </th>";
-        }
-
-        echo "</tr>";
-
-        for ($row=1; $row<4; $row++){
-
-            echo "<tr>";
-
-            for ($col=1; $col<5; $col++) {
-                
-                echo "<td>Row $row, Column $col</td>";
-            }
-            echo "</tr>";
-        }
-        
-        echo "</table>";
+        $enterprises = array(
+            'company' => array(
+                'id'      => 1,
+                'name'    => "BukaTek",
+                'address' => "Cassenda",
+                'email'   => "btk@gmail.com",
+                'number'  => 936956559,
+            )
+        );
     ?>
+
+    <div class="container">
+        <table class="table">
+            <thead>
+                <tr>
+                    <?php foreach($companyNames as $headerTitle):?>
+                        <th scope="col"><?=$headerTitle;?></th>
+                    <?php endforeach;?>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th scope="row"><?=$enterprises['company']['id'];?></th>
+                     <th><?=$enterprises['company']['name'];?></th>
+                     <td><?=$enterprises['company']['address'];?></td>
+                     <td><?=$enterprises['company']['email'];?></td>
+                     <td><?=$enterprises['company']['number'];?></td>
+                 </tr>
+            </tbody>
+        </table>
+    </div>
 </body>
 </html>
